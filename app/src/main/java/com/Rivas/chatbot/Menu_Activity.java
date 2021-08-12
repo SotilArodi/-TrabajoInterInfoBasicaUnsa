5d28ebd43cd48bb58a8ebfa5b1f58566e847e8d0
Josthin <jpilcof@unsa.edu.pe>
2021-08-12 18:57:50 -0500

    Implementacion final

--- app/src/main/java/com/Rivas/chatbot/Menu_Activity.java
+++ app/src/main/java/com/Rivas/chatbot/Menu_Activity.java
@@ -15,6 +15,11 @@ public class Menu_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         Button btnChatbot = findViewById(R.id.button6);
+        Button btnPreguntas = findViewById(R.id.button12);
+        btnPreguntas.setOnClickListener((View v) -> {
+            Intent i2 = new Intent(this, Preguntas.class);
+            startActivity(i2);
+        });
         btnChatbot.setOnClickListener((View v) -> {
             Uri link = Uri.parse(url);
             Intent i = new Intent(Intent.ACTION_VIEW,link);
